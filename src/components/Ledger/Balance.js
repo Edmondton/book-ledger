b67d29af9b02a1effe14395637933278baf63403
Edmond Chow <Edmondton@gmail.com>
2016-06-15 20:54:22 -0400

    init

---
+++ src/components/Ledger/Balance.js
@@ -0,0 +1,34 @@
+import React, { PropTypes } from 'react';
+import withStyles from 'isomorphic-style-loader/lib/withStyles';
+
+import styles from '../../styles/balance.css';
+
+function Balance({
+	amount
+}) {
+	let tableClass = amount >= 0 ? styles.positiveBalanceTable : styles.negativeBalanceTable;
+	let status = amount >= 0 ? 'Balanced' : 'Unbalanced';
+
+	return (
+		<section className={styles.container}>
+			<table className={tableClass}>
+				<tbody>
+					<tr>
+						<td className={styles.status}>Account Currently</td>
+						<td>{status}</td>
+					</tr>
+					<tr>
+						<td className={styles.balance}>Running Balance</td>
+						<td>{amount}</td>
+					</tr>
+				</tbody>
+			</table>
+		</section>
+	);
+}
+
+Balance.propTypes = {
+	amount: PropTypes.number.isRequired
+};
+
+export default withStyles(styles)(Balance);
--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -1,5 +1,8 @@
 import React, {PropTypes} from 'react';
+
 import Ledger from '../../components/Ledger/Ledger';
+import Balance from '../../components/Ledger/Balance';
+
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from '../../styles/home.css';
 
@@ -10,6 +13,7 @@ function Home({news}, context) {
 	return (
 		<div className={s.root}>
 			<div className={s.container}>
+				<Balance amount={1002} />
 				<Ledger />
 			</div>
 		</div>
